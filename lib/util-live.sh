258340dd446673cfd40f424f522fa4ff110c89d5
Philip <philm@manjaro.org>
2016-06-29 01:21:52 +0200

    [util-live] add -a switch for copying .config folder

--- lib/util-live.sh
+++ lib/util-live.sh
@@ -369,7 +369,7 @@ configure_env(){
 configure_user_root(){
 	# set up root password
 	echo "root:${password}" | chroot $1 chpasswd
-	cp /etc/skel/.{bash_profile,bashrc,bash_logout,extend.bashrc,config} /root/
+	cp -a /etc/skel/.{bash_profile,bashrc,bash_logout,extend.bashrc,config} /root/
 }
 
 configure_displaymanager_autologin(){
